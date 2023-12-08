4c13025d03fe74896c491624a6aeb0382c1ca681
AndrichSanych <andrich0105@gmail.com>
2023-12-08 20:53:42 +0200

    Finish classwork

---
+++ 01 Base/Classworks/Classwork_08.12.js
@@ -0,0 +1,26 @@
+//1
+// let text = prompt("Enter your text:");
+// let text2 = "huba sajdf sd d gdg er d r rd ";
+
+// let space = (text) => console.log(text.split(" ").length -1);
+
+// //2
+// let bigger = (text2) => console.log(text2[0].toUpperCase());
+
+// //3
+// let words = (text) => console.log(text.split(" ").length);
+
+//4
+const startWord = 'cascading style sheets';
+function makeAbbr(startWords) {
+    let resultWord = '';
+  
+    for (const char of words) {
+      if (words.charAt(char)) {
+        resultWord += char;
+      }
+    };
+  
+    return resultWord;
+  }
+
