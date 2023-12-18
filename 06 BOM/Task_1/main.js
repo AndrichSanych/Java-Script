f6f82cee13c1a842a3bd10e100fbe17762f2430d
AndrichSanych <andrich0105@gmail.com>
2023-12-18 20:48:24 +0200

    BOM

---
+++ 06 BOM/Task_1/main.js
@@ -0,0 +1,19 @@
+// Task 1
+
+// const openBtn = document.querySelector('#open');
+
+// openBtn.onclick = () => {
+//     window.open(" ", "test", "width=410,height=410");
+// };
+
+// setTimeout(()=> window.resizeTo(600,600), 2000 );
+
+// setTimeout(()=> window.moveTo(200,200), 2000 );
+
+// setTimeout(()=> window.close(), 3000);
+
+
+
+
+
+
---
+++ 06 BOM/Task_2/main.js
@@ -0,0 +1,4 @@
+let nowTime = new Date();
+
+setInterval(()=> console.log((nowTime.getHours(), nowTime.getMinutes(), nowTime.getSeconds())), 1000 );
+
